14c3c0be9e5565e9c4aa9054a22b9bf3e2e7ae0c
meji <soy@meji.es>
2021-01-01 22:10:04 +0100

    Subo server express

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.use("/src/assets", express.static(__dirname + "/src/assets/"));
+
+app.listen(process.env.PORT || 8080);
