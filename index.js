17c5df56f3237fc30051a01e9a27bf6b2028717d
meji <soy@meji.es>
2021-01-01 23:01:32 +0100

    dotenv en server

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+require("dotenv").config();
 
 app.use(express.static(__dirname + "/dist/"));
-app.use("/src/assets", express.static(__dirname + "/src/assets/"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/dist/", "index.html"));
+});
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`Server listening in  port ${process.env.PORT}`)
+);
