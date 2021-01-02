4c57824308a7fae32d680834b113e8cd5b71d6cf
meji <soy@meji.es>
2021-01-02 01:04:15 +0100

    prettier single quote

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,31 +1,31 @@
 import {Env, StringOrNumber} from './envModel';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config()
 
 class EnvBuilder {
-  private env: Env;
+  private env: Env
 
   constructor() {
-    this.env = {} as Env;
+    this.env = {} as Env
   }
 
   withPort(defaultPort: StringOrNumber) {
-    this.env.PORT = process.env.PORT || defaultPort;
-    return this;
+    this.env.PORT = process.env.PORT || defaultPort
+    return this
   }
 
   withApi(defaultApi: string) {
-    this.env.API_URL = process.env.API_URI || defaultApi;
-    return this;
+    this.env.API_URL = process.env.API_URI || defaultApi
+    return this
   }
 
   builder(): Env {
-    return this.env;
+    return this.env
   }
 }
 
 export const env = new EnvBuilder()
-  .withApi("https://denotate-back.herokuapp.com")
-  .withPort("8080")
-  .builder();
+  .withApi('https://denotate-back.herokuapp.com')
+  .withPort('8080')
+  .builder()
