6d9597cf8f8a782cb1910316793503c6eb71dc6c
meji <soy@meji.es>
2021-01-02 01:08:44 +0100

    target: 'node' en webconfig prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,39 +1,40 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
-const webpack = require("webpack");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+const webpack = require('webpack')
 
 module.exports = (env, argv) => ({
-  mode: "production",
+  mode: 'production',
   entry: {
-    app: "./src/main.ts"
+    app: './src/main.ts'
   },
+  target: 'node',
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     historyApiFallback: true
   },
-  devtool: argv.mode === "production" ? "none" : "inline-source-map",
+  devtool: argv.mode === 'production' ? 'none' : 'inline-source-map',
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-      "process.env.API_URI": JSON.stringify(process.env.API_URI)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.API_URI': JSON.stringify(process.env.API_URI)
     }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: './src/index.html'
     }),
     new CopyWebpackPlugin([
       {
-        context: "node_modules/@webcomponents/webcomponentsjs",
-        from: "**/*.js",
-        to: "webcomponents"
+        context: 'node_modules/@webcomponents/webcomponentsjs',
+        from: '**/*.js',
+        to: 'webcomponents'
       },
       {
-        from: "./src/assets/img/*",
-        to: "./",
+        from: './src/assets/img/*',
+        to: './',
         flatten: true
       }
     ]),
@@ -49,51 +50,47 @@ module.exports = (env, argv) => ({
     })
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist")
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         include: /stylesheets|node_modules/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.s[a|c]ss$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
       },
       {
         test: /\.(png|gif|jpg|cur)$/i,
-        loader: "url-loader",
+        loader: 'url-loader',
         options: { limit: 8192 }
       },
       {
         test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        loader: "url-loader",
-        options: { limit: 10000, mimetype: "application/font-woff2" }
+        loader: 'url-loader',
+        options: { limit: 10000, mimetype: 'application/font-woff2' }
       },
       {
         test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        loader: "url-loader",
-        options: { limit: 10000, mimetype: "application/font-woff" }
+        loader: 'url-loader',
+        options: { limit: 10000, mimetype: 'application/font-woff' }
       },
       {
         test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-        loader: "file-loader"
+        loader: 'file-loader'
       }
     ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: ['.tsx', '.ts', '.js']
   }
-});
+})
